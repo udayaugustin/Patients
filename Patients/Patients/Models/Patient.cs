c1d79dbd04d066f8dd3259dcf267051dcae7ed27
Uday Augustin <udayaugustin@gmail.com>
2022-08-07 17:10:26 +0530

    Comment added for the Patient View Mode and Patient domain model

--- Patients/Patients/Models/Patient.cs
+++ Patients/Patients/Models/Patient.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Patients.Models
+﻿namespace Patients.Models
 {
+    /// <summary>
+    /// A class <c>Patient</c> which represents the patient
+    /// </summary>
     public class Patient
     {
+        /// <summary>
+        /// A property which represents the name of the patient
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// A property which represents the age of the patient
+        /// </summary>
         public int Age { get; set; }
-        public string  BloodGroup { get; set; }        
+
+        /// <summary>
+        /// A property which represents the blood group of the patient
+        /// </summary>
+        public string BloodGroup { get; set; }
     }
 }
--- Patients/Patients/ViewModels/PatientViewModel.cs
+++ Patients/Patients/ViewModels/PatientViewModel.cs
@@ -2,12 +2,31 @@
 
 namespace Patients.ViewModels
 {
+    /// <summary>
+    /// A class <c>PatientViewModel<c> respresents the patient properties for the PatientTabView.
+    /// </summary>
     public class PatientViewModel : BindableBase
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string BloodGroup { get; set; }
         private bool isVisible;
+
+        /// <summary>
+        /// A property which represents the name of the patient
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// A property which represents the age of the patient
+        /// </summary>
+        public int Age { get; set; }
+        
+        /// <summary>
+        /// A property which represents the blood group of the patient
+        /// </summary>
+        public string BloodGroup { get; set; }
+
+        /// <summary>
+        /// A property which represents visibility of the patient details 
+        /// </summary>        
         public bool IsVisible
         {
             get { return isVisible; }
