bb96c2e6e18f4e993255f047e4cfa36346d0f39d
Uday Augustin <udayaugustin@gmail.com>
2022-08-07 12:58:56 +0530

    Login action added.

--- Patients/Patients/ViewModels/LoginPageViewModel.cs
+++ Patients/Patients/ViewModels/LoginPageViewModel.cs
@@ -1,16 +1,46 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Patients.ViewModels
 {
     public class LoginPageViewModel : BindableBase
     {
-        public LoginPageViewModel()
-        {
+        public DelegateCommand LoginCommand { get; set; }
 
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { SetProperty(ref username, value); }
+        }
+
+        private string password;
+        private readonly INavigationService navigationService;
+
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+
+        public LoginPageViewModel(INavigationService navigationService)
+        {
+            LoginCommand = new DelegateCommand(async () => await Login(), CanLogin).ObservesProperty(() => Username).ObservesProperty(() => Password);
+            this.navigationService = navigationService;
+        }
+
+        private bool CanLogin()
+        {
+            return !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);  
+        }
+
+        private async Task Login()
+        {
+            await navigationService.NavigateAsync("PatientsTabPage");
         }
     }
 }
--- Patients/Patients/Views/LoginPage.xaml.cs
+++ Patients/Patients/Views/LoginPage.xaml.cs
@@ -10,11 +10,6 @@ namespace Patients.Views
             InitializeComponent();
             MaterialEntry a = new MaterialEntry();
             entry.CustomTrailingIcon.WidthRequest = 60;
-        }
-
-        private void CustomEntry_Focused(object sender, FocusEventArgs e)
-        {
-
-        }
+        }        
     }
 }
